640bb3142b68463507ac230d6d5f370c2c42e95e
peterdalle <peterdalle@gmail.com>
2020-07-01 19:58:01 +0200

    Use TestSpider instead

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -9,17 +9,31 @@ srcdir = "../mechanicalnews"
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
 import unittest
-from pipelines import MySQLPipeline
-from spiders.aftonbladet import AftonbladetSpider
+from scrapy.spiders import CrawlSpider
 from scrapy.crawler import Crawler
+from pipelines import MySQLPipeline
+from basespider import BaseArticleSpider
+
+
+class TestSpider(BaseArticleSpider):
+    name = "testspider"
+    allowed_domains = []
+    start_urls = []
+    SPIDER_GUID = "test"
+    LAST_UPDATED = "2020-06-24"
+    DEFAULT_LANGUAGE = "en"
+
+    def parse_article(self):
+        pass
 
 
 class RunTest_MySQLPipeline(unittest.TestCase):
 
     def test_crawler_default_settings(self):
         pipeline = MySQLPipeline.from_crawler(Crawler(
-            spidercls=AftonbladetSpider))
+            spidercls=TestSpider))
         self.assertEqual(pipeline.database, "mechanicalnews")
+        del(pipeline)
 
 
 if __name__ == '__main__':
